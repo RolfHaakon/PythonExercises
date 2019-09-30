24ca6161a4c5fe494055588897a6c3ab343ad40d
Rolf <rolfhaakon.rokseth@my.jcu.edu.au>
2019-09-30 23:18:34 +1000

    - Updated NeuralNetwork.py

--- Exercises/NeuralNetwork.py
+++ Exercises/NeuralNetwork.py
@@ -1,6 +1,6 @@
 """
 Neural network from https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
-
+https://www.kdnuggets.com/2018/10/simple-neural-network-python.html
 """
 
 
